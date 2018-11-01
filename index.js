e07ba3a8afe628851544b6152970f71deb379454
Brandon Plaster <brandon@littlstar.com>
2018-11-01 12:25:08 -0400

    refactor(index): removed state. Added ability to pass conf function. Fixed base.root config.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { resolve } = require('path')
 const extend = require('extend')
-const debug = require('debug')('ara:runtime:configuration')
 const rc = require('rc')
 const os = require('os')
 
@@ -15,18 +14,20 @@ const ARA_DIR = resolve(os.homedir(), '.ara')
  */
 
 const RUNTIME_CONFIGURATION_DEFAULTS = {
-  network: {
-    identity: {
-      root: resolve(ARA_DIR, 'identities')
-    }
-  },
+  network: { },
   data: { root: ARA_DIR },
   web3: { }
 }
 
-const state = {}
+const NETWORK_CONFIGURATION_EXTENSION = base => ({
+  network: {
+    identity: {
+      root: resolve(base.data.root, 'identities')
+    }
+  }
+})
 
-module.exports = ARARuntimeConfiguration
+module.exports = AraRuntimeConfiguration
 
 /**
  * Return runtime configuration found in the nearest
@@ -37,21 +38,22 @@ module.exports = ARARuntimeConfiguration
  * @return {Object}
  * @see
  */
-function ARARuntimeConfiguration(conf, name) {
-  if (undefined !== conf && null !== conf && 'object' !== typeof conf) {
-    throw new TypeError('Expecting configuration to be an object.')
+function AraRuntimeConfiguration(conf, name) {
+  if (undefined !== conf && null !== conf && 'object' !== typeof conf && 'function' !== typeof conf) {
+    throw new TypeError('Expecting configuration to be an object or function.')
   } else if (name && 'string' !== typeof name) {
     throw new TypeError('Expecting name to be a string.')
   } else {
-    const defaults = RUNTIME_CONFIGURATION_DEFAULTS
     // use an empty argv because the 'rc' module uses
     // 'process.argv' if we don't give it one
     const argv = {}
     // eslint-disable-next-line no-param-reassign
     name = name || RUNTIME_CONFIGURATION_NAME
-    // eslint-disable-next-line no-param-reassign
-    conf = rc(name, extend(true, {}, defaults, conf), argv)
-    extend(true, state, conf)
-    return state
+
+    const baseConf = extend(true, {}, rc(name, RUNTIME_CONFIGURATION_DEFAULTS, argv))
+    extend(true, baseConf, NETWORK_CONFIGURATION_EXTENSION(baseConf))
+    extend(true, baseConf, ('function' === typeof conf) ? conf(baseConf) : conf)
+
+    return baseConf
   }
 }
