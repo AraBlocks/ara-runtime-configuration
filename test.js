f473f7c3b07f38490ae6b89411bb57fc48fb7e9c
Brandon Plaster <brandon@littlstar.com>
2018-11-01 13:15:00 -0400

    test(): added test for function

--- test.js
+++ test.js
@@ -11,6 +11,20 @@ test.cb('simple', (t) => {
 
   t.true('function' === typeof rc)
   t.true('object' === typeof rc())
+  t.true('object' === typeof rc(() => {}))
   t.true(123 === rc({ foo: 123 }).foo)
   t.end()
+})
+
+test('function.valid', (t) => {
+  const conf = rc((base) => {
+    t.true(Boolean(base.network.identity.root))
+    t.true(Boolean(base.data.root))
+    return {
+      test: {
+        root: base.data.root
+      }
+    }
+  })
+  t.true(Boolean(conf.test.root) && (conf.test.root === conf.data.root))
 })
