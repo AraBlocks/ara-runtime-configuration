d8cfc5c898385d591c4060be35fbba348ea04cdf
Joseph Werle <joseph.werle@gmail.com>
2018-11-29 17:07:36 -0500

    refactor(browser.js): Stub out config for browserify

--- browser.js
+++ browser.js
@@ -1,3 +1,15 @@
+const extend = require('extend')
+
 module.exports = function rc(conf) {
-  return Object.assign({}, conf)
+  const root = extend(true, {
+    network: { identity: { } },
+    data: { root: '/.ara' },
+    web3: { }
+  }, conf)
+
+  return extend(true, {
+    network: {
+      identity: `${root.data.root}/identities}`
+    }
+  }, root)
 }
